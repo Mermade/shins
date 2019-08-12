2d1c857d7d81cd5b6e7497a6c0bb61f3244e7df1
Mike Ralphson <mike.ralphson@gmail.com>
2019-08-12 15:06:27 +0100

    fix: use path.resolve for layout option, refs #73

--- index.js
+++ index.js
@@ -391,7 +391,7 @@ function render(inputStr, options, callback) {
 
         var ejsOptions = {};
         ejsOptions.debug = false;
-        ejs.renderFile(path.join(globalOptions.root, options.layout || '/source/layouts/layout.ejs'), locals, ejsOptions, function (err, str) {
+        ejs.renderFile(path.resolve(globalOptions.root, options.layout || 'source/layouts/layout.ejs'), locals, ejsOptions, function (err, str) {
             if (err) reject(err)
             else resolve(str);
         });
